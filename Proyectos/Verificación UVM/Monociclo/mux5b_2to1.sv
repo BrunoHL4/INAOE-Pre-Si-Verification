386b6f825662c28f448f76cb790122df668592e0
Bruno Hernandez Lopez <108531396+BrunoHL4@users.noreply.github.com>
2022-07-20 13:52:09 -0500

    Delete Proyectos/Verificación UVM/Monociclo directory

--- Proyectos/Verificación UVM/Monociclo/mux5b_2to1.sv
+++
@@ -1,8 +0,0 @@
-module mux5b_2to1 (Z,Sel,X,Y);   //////sel=1,z=X    sel=0,z=Y
-  parameter WID = 5;
-  input wire [WID-1:0] X, Y;
-  input wire Sel;
-  output wire [WID-1:0] Z;
-  
-  assign Z= Sel ? X:Y;
-endmodule
